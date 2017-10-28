c6280a4b097f90264d24567561aa0ad4da877992
Kojo Appiah <kojo.e.appiah@gmail.com>
2017-10-28 17:30:27 -0400

    Finished Horizontal Search Test

--- app/WordSearch.js
+++ app/WordSearch.js
@@ -3,23 +3,76 @@ fs = require('fs')
 class WordSearch {
 
     constructor(inputFile) {
+
+        this.words_found = [];
         var file = fs.readFileSync(inputFile, "utf8");
         this.words_to_find = file.substr(0, file.indexOf("\n"));
         this.words_to_find = this.words_to_find.replace("\r", "");
         this.words_to_find = this.words_to_find.split(",");
 
-        this.search_space = file.substr(file.indexOf("\n")+1, file.length);
-        this.search_space = this.search_space.replace(/,/g , "");
+        this.search_space = file.substr(file.indexOf("\n") + 1, file.length);
+        this.search_space = this.search_space.replace(/,/g, "");
         this.search_space = this.search_space.split("\r\n");
 
     }
 
-    words() {
+    getWordsToFind() {
         return this.words_to_find;
     }
 
     searchSpace() {
         return this.search_space;
+    }
+
+    getWordsFound() {
+        return this.words_found;
+    }
+
+    searchHorizontal() {
+
+        var extend;
+        var wordstub = "";
+        var length = 0;
+
+        for(var y = 0; y < this.search_space.length; y++) {
+            for (var x = 0; x < this.search_space.length; x++) {
+                extend = false;
+                wordstub += this.search_space[y][x];
+                for (var i = 0; i < this.words_to_find.length; i++) {
+                    if (this.words_to_find[i].substr(0, wordstub.length) === wordstub) {
+                        extend = true;
+                    }
+                    if (this.words_to_find[i] === wordstub)
+                        this.words_found.push(this.words_to_find[i]);
+
+                }
+
+                if (wordstub.length > length)
+                    length = wordstub.length;
+
+                if (!extend)
+                    wordstub = "";
+            }
+        }
+
+        for (var y = 0; y < this.search_space.length; y++) {
+            for (var x = this.search_space.length - 1; x >= 0; x--) {
+                extend = false;
+                wordstub += this.search_space[y][x];
+                for (var i = 0; i < this.words_to_find.length; i++) {
+                    if (this.words_to_find[i].substr(0, wordstub.length) === wordstub) {
+                        extend = true;
+                    }
+                    if (this.words_to_find[i] === wordstub)
+                        this.words_found.push(this.words_to_find[i]);
+
+                }
+
+                if (!extend)
+                    wordstub = "";
+            }
+        }
+
     }
 
 }
--- spec/word-search-spec.js
+++ spec/word-search-spec.js
@@ -5,7 +5,7 @@ WordSearch = require("../app/WordSearch.js");
 describe("Acquire Words to Search For", function () {
     it("should parse the first line of the input file", function () {
         var wordsearch = new WordSearch('./app/inputFile.txt');
-        expect(wordsearch.words()).toMatch(["BONES","KHAN","KIRK","SCOTTY","SPOCK","SULU","UHURA"]);
+        expect(wordsearch.getWordsToFind()).toMatch(["BONES","KHAN","KIRK","SCOTTY","SPOCK","SULU","UHURA"]);
     });
 });
 
@@ -13,5 +13,13 @@ describe("Create a 2 dimensional array of characters to represent the word searc
     it("should print the second row from the top", function () {
         var wordsearch = new WordSearch('./app/inputFile.txt');
         expect(wordsearch.searchSpace()[1]).toMatch("LLSHKZZWZCGJUYG");
+    });
+});
+
+describe("Search every line horizontally", function () {
+    it("should find the words 'SCOTTY' and 'PICARD'", function () {
+        var wordsearch = new WordSearch('./app/inputFile.txt');
+        wordsearch.searchHorizontal()
+        expect(wordsearch.getWordsFound()).toEqual(["SCOTTY", "KIRK"]);
     });
 });
