2cca2cb27ce39625c4b0b2009867f941a927ae8a
Kojo Appiah <kojo.e.appiah@gmail.com>
2017-10-28 14:36:06 -0400

    Finished Input WordSearch Test

--- app/WordSearch.js
+++ app/WordSearch.js
@@ -7,10 +7,19 @@ class WordSearch {
         this.words_to_find = file.substr(0, file.indexOf("\n"));
         this.words_to_find = this.words_to_find.replace("\r", "");
         this.words_to_find = this.words_to_find.split(",");
+
+        this.search_space = file.substr(file.indexOf("\n")+1, file.length);
+        this.search_space = this.search_space.replace(/,/g , "");
+        this.search_space = this.search_space.split("\r\n");
+
     }
 
     words() {
         return this.words_to_find;
+    }
+
+    searchSpace() {
+        return this.search_space;
     }
 
 }
--- spec/word-search-spec.js
+++ spec/word-search-spec.js
@@ -8,3 +8,10 @@ describe("Acquire Words to Search For", function () {
         expect(wordsearch.words()).toMatch(["BONES","KHAN","KIRK","SCOTTY","SPOCK","SULU","UHURA"]);
     });
 });
+
+describe("Create a 2 dimensional array of characters to represent the word search", function () {
+    it("should print the second row from the top", function () {
+        var wordsearch = new WordSearch('./app/inputFile.txt');
+        expect(wordsearch.searchSpace()[1]).toMatch("LLSHKZZWZCGJUYG");
+    });
+});
